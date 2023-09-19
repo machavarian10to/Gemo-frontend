4b52bd2d691385a86d80234191023be2ccbe1d14
machavarian10to <otar.matchavariani.1@btu.edu.ge>
2023-09-19 20:06:46 +0400

    wip: create post editor

--- src/components/UserHome/Tabs/PostTabContent.jsx
+++ src/components/UserHome/Tabs/PostTabContent.jsx
@@ -1,4 +1,11 @@
 import FormatBoldIcon from '@mui/icons-material/FormatBold';
+import FormatItalicIcon from '@mui/icons-material/FormatItalic';
+import FormatUnderlinedIcon from '@mui/icons-material/FormatUnderlined';
+import FormatListBulletedIcon from '@mui/icons-material/FormatListBulleted';
+import FormatListNumberedIcon from '@mui/icons-material/FormatListNumbered';
+import FormatQuoteIcon from '@mui/icons-material/FormatQuote';
+import InsertLinkIcon from '@mui/icons-material/InsertLink';
+import TitleIcon from '@mui/icons-material/Title';
 
 function PostTabContent() {
   return (
@@ -7,9 +14,30 @@ function PostTabContent() {
         <button>
           <FormatBoldIcon style={{ color: 'grey' }} />
         </button>
+        <button>
+          <FormatItalicIcon style={{ color: 'grey' }} />
+        </button>
+        <button>
+          <FormatUnderlinedIcon style={{ color: 'grey' }} />
+        </button>
+        <button>
+          <TitleIcon style={{ color: 'grey' }} />
+        </button>
+        <button>
+          <FormatListBulletedIcon style={{ color: 'grey' }} />
+        </button>
+        <button>
+          <FormatListNumberedIcon style={{ color: 'grey' }} />
+        </button>
+        <button>
+          <InsertLinkIcon style={{ color: 'grey' }} />
+        </button>
+        <button>
+          <FormatQuoteIcon style={{ color: 'grey' }} />
+        </button>
       </div>
       <textarea
-        placeholder='Add any additional info if you want...'
+        placeholder='Additional info (optional)'
         maxLength={200}
       ></textarea>
     </div>
