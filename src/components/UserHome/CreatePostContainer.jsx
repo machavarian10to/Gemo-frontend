5d3b68e32bbf29056a76b1701ec19295d06488d9
machavarian10to <otar.matchavariani.1@btu.edu.ge>
2023-09-20 23:05:37 +0400

    feat: add character counter on post title

--- src/components/UserHome/CreatePostContainer.jsx
+++ src/components/UserHome/CreatePostContainer.jsx
@@ -4,17 +4,34 @@ import CollectionsIcon from '@mui/icons-material/Collections';
 import PollIcon from '@mui/icons-material/Poll';
 import EditCalendarIcon from '@mui/icons-material/EditCalendar';
 import GifBoxIcon from '@mui/icons-material/GifBox';
-import PostTabContent from '@/components/UserHome/Tabs/PostTabContent';
-import MediaTabContent from '@/components/UserHome/Tabs/MediaTabContent';
-import PollTabContent from '@/components/UserHome/Tabs/PollTabContent';
-import EventTabContent from '@/components/UserHome/Tabs/EventTabContent';
-import GifTabContent from '@/components/UserHome/Tabs/GifTabContent';
+import PostTabContent from '@/components/UserHome/post-modal-tabs/PostTabContent';
+import MediaTabContent from '@/components/UserHome/post-modal-tabs/MediaTabContent';
+import PollTabContent from '@/components/UserHome/post-modal-tabs/PollTabContent';
+import EventTabContent from '@/components/UserHome/post-modal-tabs/EventTabContent';
+import GifTabContent from '@/components/UserHome/post-modal-tabs/GifTabContent';
 
 export default function CreatePostContainer() {
   const [activeTab, setActiveTab] = useState('post');
+  const [postTitle, setPostTitle] = useState('');
+  const [charCount, setCharCount] = useState(0);
 
   function handleActiveTab(tabName) {
     setActiveTab(tabName);
+  }
+
+  function setTitle(e) {
+    setCharCount(e.target.value.length);
+    setPostTitle(e.target.value);
+
+    if (e.target.value.length <= 50) {
+      e.target.style.height = '35px';
+    } else if (e.target.value.length > 50 && e.target.value.length <= 100) {
+      e.target.style.height = '55px';
+    } else if (e.target.value.length > 100 && e.target.value.length <= 150) {
+      e.target.style.height = '65px';
+    } else if (e.target.value.length > 150 && e.target.value.length <= 200) {
+      e.target.style.height = '85px';
+    }
   }
 
   return (
@@ -62,11 +79,16 @@ export default function CreatePostContainer() {
       </div>
 
       <div className='active-tab-content'>
-        <textarea
-          className='title'
-          placeholder='Post Title'
-          maxLength={200}
-        ></textarea>
+        <div className='post-title-wrapper'>
+          <textarea
+            className='title'
+            placeholder='Post Title'
+            maxLength={200}
+            onChange={(e) => setTitle(e)}
+            value={postTitle}
+          ></textarea>
+          <span>{charCount}/200</span>
+        </div>
 
         {activeTab === 'post' ? (
           <PostTabContent />
