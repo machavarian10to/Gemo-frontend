8476a70f75eef9ccf86104f636e4b8974bd61e93
machavarian10to <otar.matchavariani.1@btu.edu.ge>
2024-02-10 19:50:51 +0400

    wip: create image changer time bar

--- src/components/pages/Authorization/ImagesCarousel.jsx
+++ src/components/pages/Authorization/ImagesCarousel.jsx
@@ -4,39 +4,60 @@ import axios from 'axios';
 function ImagesCarousel() {
   const [images, setImages] = useState([]);
   const [currentImage, setCurrentImage] = useState(0);
-  const [loaded, setLoaded] = useState(false);
+  const [loading, setLoading] = useState(true);
+  const [width, setWidth] = useState(0);
 
   useEffect(() => {
     axios
       .get(
         `https://pixabay.com/api/?key=${
           import.meta.env.VITE_PIXABAY_API_KEY
-        }&q=food&image_type=photo&per_page=200&orientation=vertical&category=food`,
+        }&q=delicious+food&image_type=photo&per_page=200&orientation=vertical&category=food&safesearch=true&editors_choice=true`,
       )
       .then((res) => {
         const shuffledImages = res.data.hits.sort(() => Math.random() - 0.5);
         setImages(shuffledImages);
-        setLoaded(false);
+        setLoading(false);
+        setWidth(0);
       })
       .catch((err) => console.log(err));
   }, []);
 
   useEffect(() => {
+    const int = setInterval(() => {
+      setWidth((prev) => (prev === 100 ? 0 : prev + 1));
+    }, 50);
+
     const interval = setInterval(() => {
       setCurrentImage((prev) => (prev === images.length - 1 ? 0 : prev + 1));
-    }, 10000);
+    }, 5000);
 
-    return () => clearInterval(interval);
+    return () => {
+      clearInterval(int);
+      clearInterval(interval);
+    };
   }, [images.length]);
 
   return (
     <div className='user-home__auth-food-image-wrapper'>
-      <img
-        src={images[currentImage]?.webformatURL}
-        alt='food'
-        className={`user-home__auth-food-image ${loaded ? 'loaded' : ''}`}
-        onLoad={() => setLoaded(true)}
-      />
+      {loading ? (
+        // TODO: Add a loader
+        <div className='loader'>loading</div>
+      ) : (
+        <div
+          className='user-home__auth-food-image'
+          style={{
+            backgroundImage: `url(${images[currentImage].largeImageURL})`,
+          }}
+        >
+          <div
+            style={{
+              width: `${width}%`,
+            }}
+            className='user-home__auth-food-time-bar'
+          ></div>
+        </div>
+      )}
     </div>
   );
 }
