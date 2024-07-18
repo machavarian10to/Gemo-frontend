1890d8d33d02166503740862ce672379df551f08
machavarian10to <otar.matchavariani.1@btu.edu.ge>
2024-07-18 19:51:50 +0400

    fix: see gem reacts tranisition

--- src/components/pages/UserHome/user-gem/comments/CommentSection.jsx
+++ src/components/pages/UserHome/user-gem/comments/CommentSection.jsx
@@ -2,8 +2,22 @@ import AddComment from '@/components/pages/UserHome/user-gem/comments/AddComment
 import Comment from '@/components/pages/UserHome/user-gem/comments/Comment';
 import Fade from '@mui/material/Fade';
 import PropTypes from 'prop-types';
+import { useState } from 'react';
 
 function CommentSection({ gem }) {
+  const [comments, setComments] = useState(gem.comments.slice(0, 2));
+  const [showMoreComments, setShowMoreComments] = useState(false);
+
+  console.log('comments', comments);
+  function onClickShowMoreComments() {
+    if (showMoreComments) {
+      setComments(gem.comments.slice(0, 2));
+    } else {
+      setComments(gem.comments);
+    }
+    setShowMoreComments(!showMoreComments);
+  }
+
   return (
     <>
       <Fade in={true} timeout={600}>
@@ -17,9 +31,13 @@ function CommentSection({ gem }) {
               ))}
             </div>
           )}
+
           {gem.comments.length > 2 && (
-            <div className='user-gem__comment-show-more-comments'>
-              show more comments
+            <div
+              className='user-gem__comment-show-more-comments'
+              onClick={onClickShowMoreComments}
+            >
+              {showMoreComments ? 'Show less comments' : 'Show more comments'}
             </div>
           )}
         </div>
