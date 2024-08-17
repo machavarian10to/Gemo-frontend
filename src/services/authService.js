91fb5c0a96830dda6b1ee01f4059cd6397d78710
machavarian10to <otar.matchavariani.1@btu.edu.ge>
2024-08-17 14:07:58 +0400

    feat: move refresh token renew in backend

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,4 +1,4 @@
-import axios from 'axios';
+import axiosInstance from '@/services/axios';
 
 const setToken = (name, value) => {
   document.cookie = `${name}=${value}; HttpOnly; Secure`;
@@ -10,38 +10,12 @@ const getToken = (name) => {
   if (parts.length === 2) return parts.pop().split(';').shift();
 };
 
-const renewAccessToken = async () => {
-  const refreshToken = getToken('refreshToken');
-  const response = await axios.post(
-    `${import.meta.env.VITE_API_URL}/auth/refresh-token`,
-    { refreshToken },
-  );
-  setToken('accessToken', response.data.accessToken);
-  setToken('refreshToken', response.data.refreshToken);
-};
-
 const getCurrentUser = async () => {
   try {
-    const response = await axios.get(
-      `${import.meta.env.VITE_API_URL}/auth/get-user`,
-      {
-        headers: {
-          Authorization: `Bearer ${getToken('accessToken')}`,
-        },
-      },
-    );
+    const response = await axiosInstance.get('/auth/get-user');
     return response.data;
   } catch (error) {
-    await renewAccessToken();
-    const response = await axios.get(
-      `${import.meta.env.VITE_API_URL}/auth/get-user`,
-      {
-        headers: {
-          Authorization: `Bearer ${getToken('accessToken')}`,
-        },
-      },
-    );
-    return response.data;
+    console.log(error);
   }
 };
 
