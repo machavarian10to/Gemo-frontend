33cd2df9753284f6b4c18204d5cc97f6cfcdb5ea
machavarian10to <otar.matchavariani.1@btu.edu.ge>
2024-08-27 20:03:08 +0400

    refactor: user email verification

--- src/components/pages/Authorization/EmailVerification.jsx
+++ src/components/pages/Authorization/EmailVerification.jsx
@@ -1,9 +1,10 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
 
 function EmailVerification() {
   const navigate = useNavigate();
+  const [verified, setVerified] = useState('');
 
   useEffect(() => {
     async function verifyEmail() {
@@ -11,20 +12,35 @@ function EmailVerification() {
       if (!token) return;
 
       try {
-        const res = await axios.get(
-          `${import.meta.env.VITE_API_URL}/auth/email-verify?token=${token}`,
-        );
-        if (res.status === 200) {
-          localStorage.setItem('emailVerified', 'true');
-        }
+        // const res = await axios.get(
+        //   `${import.meta.env.VITE_API_URL}/auth/email-verify?token=${token}`,
+        // );
+        // if (res.status === 200) {
+        setVerified('Successful');
+        // }
       } catch (err) {
         console.log(err);
+        setVerified('Failed');
       } finally {
-        navigate('/');
+        // navigate('/');
       }
     }
     verifyEmail();
   }, [navigate]);
+
+  return (
+    <>
+      {verified && (
+        <div className='email-verification-wrapper'>
+          <div className='email-verification-container'>
+            <h4 className={`verify-state-${verified}`}>
+              Email verification is {verified}!
+            </h4>
+          </div>
+        </div>
+      )}
+    </>
+  );
 }
 
 export default EmailVerification;
